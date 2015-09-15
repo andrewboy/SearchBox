cc4d3475f166d1adba051e41b88a351364535896
unknown <beck.andras@989096CE5198.phgroup.local>
2015-09-15 15:45:51 +0200

    add url param handler plugin

--- assets/js/jquery.searchBox.js
+++ assets/js/jquery.searchBox.js
@@ -1,4 +1,52 @@
 ;(function($) {
+    
+    function Util(){}
+    
+    Util.getParam = function(){
+        
+        var params = new Object;
+        
+        var setParam = function(params, paramIndexes, value){
+            var realParam = paramIndexes.shift();
+            var cleanedParam = realParam.match(/\[(.*?)\]/)[1];
+            
+            if( "undefined" === typeof(params[cleanedParam]) ){
+                params[cleanedParam] = [];
+            }
+            
+            if( paramIndexes.length > 0 ){
+                setParam(params[cleanedParam], paramIndexes, value);
+            } else{
+                if(realParam === '[]'){
+                    params.push(value);
+                } else{
+                    params[cleanedParam] = value;
+                }
+            }
+            
+        };
+        
+        var searchParams = document.location.search.substr(1);
+        var arrSearchParams = decodeURI(searchParams).split('&');
+        
+        if(searchParams.length < 1 || arrSearchParams.length < 1){return params;}
+        
+        for(var i in arrSearchParams){
+            var param = arrSearchParams[i].split('=');
+            var realIdx = param[0].replace(/\[.*?\]/g, '');
+            
+            if(param[0].search(/\[(.*?)\]/g)> -1){
+                if("undefined" === typeof(params[realIdx])){
+                    params[realIdx] = [];
+                }
+                setParam(params[realIdx], param[0].match(/\[(.*?)\]/g), decodeURIComponent(param[1]));
+            } else{
+                params[ param[0] ] = decodeURIComponent(param[1]);
+            }
+        }
+        
+        return params;
+    };
 
     var SearchItem = function(context, id, params){
         var context = context;
---
+++ assets/js/jquery.urlParam.js
@@ -0,0 +1,60 @@
+(function($){
+    
+    $.fn.getParams = function(){
+        var params = new Object;
+        
+        var setParam = function(params, paramIndexes, value){
+            var realParam = paramIndexes.shift();
+            var cleanedParam = realParam.match(/\[(.*?)\]/)[1];
+            
+            if( "undefined" === typeof(params[cleanedParam]) ){
+                params[cleanedParam] = [];
+            }
+            
+            if( paramIndexes.length > 0 ){
+                setParam(params[cleanedParam], paramIndexes, value);
+            } else{
+                if(realParam === '[]'){
+                    params.push(value);
+                } else{
+                    params[cleanedParam] = value;
+                }
+            }
+            
+        };
+        
+        var searchParams = document.location.search.substr(1);
+        var arrSearchParams = decodeURI(searchParams).split('&');
+        
+        if(searchParams.length < 1 || arrSearchParams.length < 1){return params;}
+        
+        for(var i in arrSearchParams){
+            var param = arrSearchParams[i].split('=');
+            var realIdx = param[0].replace(/\[.*?\]/g, '');
+            
+            if(param[0].search(/\[(.*?)\]/g)> -1){
+                if("undefined" === typeof(params[realIdx])){
+                    params[realIdx] = [];
+                }
+                setParam(params[realIdx], param[0].match(/\[(.*?)\]/g), decodeURIComponent(param[1]));
+            } else{
+                params[ param[0] ] = decodeURIComponent(param[1]);
+            }
+        }
+        
+        return params;
+    };
+    
+    $.fn.hasParam = function(paramName){
+        var params = $.fn.getParams();
+        
+        return "undefined" === typeof(params[paramName]);
+    };
+    
+    $.fn.getParam = function(paramName){
+        var params = $.fn.getParams();
+        
+        return params[paramName] || null;
+    };
+    
+})(jQuery);
